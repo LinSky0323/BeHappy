10a6571705bbbca30c84450a5b32149dc8941706
LinSky0323 <a3822584@gmail.com>
2024-08-29 10:50:11 +0800

    0829_FixFont

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,10 @@ import { Inter,Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
 import localFont from 'next/font/local'
 
-const myFont = localFont({ src: [
-  {path:"./kurobara-gothic-regular.ttf",weight:"400",style:"normal"},
-  {path:"./kurobara-gothic-medium.ttf",weight:"500",style:"normal"},
-  {path:"./kurobara-gothic-bold.ttf",weight:"700",style:"normal"}
-],fallback:['Noto Sans TC', 'sans-serif'],variable:"--font-myfont" })
-// const notoSans = Noto_Sans_TC({ weight: ['400', '700'], subsets: ["latin"]})
+// const myFont = localFont({ src: [
+//   {path:"./XiangcuiKesong.ttf"}
+// ],fallback:['Noto Sans TC', 'sans-serif'],variable:"--font-myfont" })
+const notoSans = Noto_Sans_TC({ weight: ['400', '700'], subsets: ["latin"]})
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,7 +22,7 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
           <link rel="icon" href="/motorcycle.png" />
 
         </head>
-      <body className={myFont.className}>
+      <body className={notoSans.className}>
           {children}
       </body>
     </html>
