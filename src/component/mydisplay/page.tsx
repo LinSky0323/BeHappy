c0955153efaa5cd5143420d6c46ae5063215e78b
LinSky0323 <a3822584@gmail.com>
2024-09-01 05:46:43 +0800

    0901_animate1

--- src/component/mydisplay/page.tsx
+++ src/component/mydisplay/page.tsx
@@ -1,10 +1,58 @@
+"use client"
 
-
+import { useEffect, useState } from "react"
 import styles from "./page.module.css" 
+import { useSpring, useSprings,animated,config } from "@react-spring/web"
+import Image from "next/image"
+
+const DisplayBlock = [{content:`使用者:\n展示資料、作品。\n設定預約內容。\n獲得網頁。\n後台管理訂單。`,},{content:"顧客：\n瀏覽服務。\n預約服務。\n後台查看預約狀態",}]
+
+
 
 export default function MyDisplay(){
+    const [toggle] = useState(new Set())
+    const [props,api] = useSprings(2,(i)=>({from:{opacity:i?1:0}}))
+    useEffect(()=>{
+        api.start(i=>{
+            const opacity = i?0:1
+            return{
+                opacity,
+                loop:{
+                    reverse:true
+                },
+                config: config.slow,
+                delay:3000 
+            }
+        })
+    },[])
+
     return(
         <section className={styles.container}>
-                
+            {props.map(({opacity},index)=>{
+                if(index===0){
+                    return(
+                        <animated.div className={styles.content} style={{opacity}} key={index}>
+                            <div className={styles.text}>{DisplayBlock[index].content.split('\n').map((line,i)=>(<span key={i}>{line}<br/></span>))}</div>
+                            <div className={styles.img}>
+                                <div className={styles.ImgContainer}><Image alt="預覽" src={"/trade_display.png"} fill sizes="100%" style={{ objectFit: 'cover' }}/></div>
+                                <div className={styles.ImgContainer}><Image alt="預覽" src={"/trade_item.png"} fill sizes="100%" style={{ objectFit: 'cover' }}/></div>
+                                <div className={styles.ImgContainer}><Image alt="預覽" src={"/trade_time.png"} fill sizes="100%" style={{ objectFit: 'cover' }}/></div>
+                                <div className={styles.ImgContainer}><Image alt="預覽" src={"/trade_booking.png"} fill sizes="100%" style={{ objectFit: 'cover' }}/></div>
+                            </div>
+                        </animated.div>
+                    )
+                }
+                else{
+                    return(
+                        <animated.div className={styles.content} style={{opacity}} key={index}>
+                            <div className={styles.img}>
+                                <div className={styles.GImgContainer}><Image alt="預覽" src={"/guest_page.png"} fill sizes="100%" style={{ objectFit: 'cover' }}/></div>
+                                <div className={styles.GImgContainer}><Image alt="預覽" src={"/guest_booking.png"} fill sizes="100%" style={{ objectFit: 'cover' }}/></div>
+                            </div>
+                            <div className={styles.text}>{DisplayBlock[index].content.split('\n').map((line,i)=>(<span key={i}>{line}<br/></span>))}</div>
+                        </animated.div>
+                    )
+                }
+            })}
         </section>
     )}
--- src/component/teachtrade/page.tsx
+++ src/component/teachtrade/page.tsx
@@ -1,9 +1,86 @@
-
+"use client"
+import { useEffect, useState } from "react"
 import styles from "./page.module.css" 
+import { useSprings,animated,to as interpolate } from "@react-spring/web"
+import { useDrag } from "@use-gesture/react"
+import Image from "next/image"
+
+const cards = [{src:"/trade_display.png"},{src:"/trade_time.png"}]
+
+
 
 export default function TeachTrade(){
+    const to = (i: number) => ({
+        x: 50,
+        y: 20,
+        scale: 1,
+        rot: 0,
+        delay: i * 100,
+      })
+    const from = (_i: number) => ({ x: -1500, rot: 0, scale: 1.5, y:0})
+    
+    const trans = (r: number, s: number) =>
+      `perspective(1500px)  rotateY(${r / 10}deg) rotateZ(${r/2}deg) scale(${s})`
+    
+    const [gone] = useState(()=>new Set())
+    const [props,api] = useSprings(cards.length,(i)=>({
+        from:from(i)
+    }))
+    useEffect(()=>{
+        api.start((i)=>(to(i)))
+    },[])
+    const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir],velocity:[vx]},)=>{
+        // if(xDir>0)return
+        const trigger = vx > 0.2
+        if(!down && trigger) gone.add(index);
+        api.start((i)=>{
+            if (index !== i) return
+            const isGone = gone.has(index)
+            const x = isGone?(200+window.innerWidth):down?mx+50:50;
+            const rot = mx/100
+            const scale = down?1.1:1
+            return({
+                x,
+                rot,
+                scale,
+                delay: undefined,
+                config:{ friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
+            })
+        })
+        if(!down && gone.size === cards.length){
+            setTimeout(()=>{
+                gone.clear()
+                api.start((i)=>to(i))
+            },600)
+        }
+    }) 
+    const clickRow = ()=>{
+        gone.add(gone.size+1)
+        api.start((i)=>{
+            if (cards.length-gone.size !== i) return
+            return({
+                x:200+window.innerWidth,
+                rot:10,
+                scale:1.1,
+                delay: undefined,
+                config:{ friction: 50, tension: 200}
+            })
+        })
+        if(gone.size === cards.length){
+            setTimeout(()=>{
+                gone.clear()
+                api.start((i)=>to(i))
+            },600)
+        }
+    }
     return(
     <section className={styles.container}>
-            
+            <div className={styles.title}>使用引導</div>
+            <div className={styles.row} onClick={clickRow}>向右滑動<br/>{">>>"}</div>
+            {props.map(({x,y,rot,scale},index)=>(       
+                 <animated.div key={index}  className={styles.card} {...bind(index)} style={{x,y,transform: interpolate([rot, scale], trans),}}>
+                     <div className={styles.imgContainer} style={{touchAction:"none",pointerEvents: "none"}}><Image  alt="教學"src={cards[index].src} fill sizes="100%" style={{objectFit:"cover"}}/></div>
+                 </animated.div>
+            ))}
     </section>
 )}
