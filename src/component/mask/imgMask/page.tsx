1a35cd5e27085d65f638d8afbb38942bdfc65f93
LinSky0323 <a3822584@gmail.com>
2024-09-03 04:28:50 +0800

    0903_imgMask

---
+++ src/component/mask/imgMask/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { StopPropogation } from "@/lib/stopPropagation"
+import styles from "./page.module.css"
+import { SetStateAction } from "react"
+import Image from "next/image"
+
+export default function ImgMask({setOpen,src}:{setOpen:React.Dispatch<SetStateAction<boolean>>,src:string}){
+    const handleClick = ()=>{
+        setOpen(false)
+    }
+    return(
+        <div className={styles.mask} onClick={handleClick}>
+            <div className={styles.container} onClick={StopPropogation}>
+                <Image alt="大圖" src={src} fill sizes="100%" style={{objectFit:"cover"}}/>
+            </div>
+        </div>
+    )
+}
--- src/component/productCard/productCard.tsx
+++ src/component/productCard/productCard.tsx
@@ -2,12 +2,21 @@
 
 import Image from "next/image"
 import styles from "./productCard.module.css"
+import { useState } from "react"
+import ImgMask from "../mask/imgMask/page"
 
 export default function ProductCard({description,src}:{description:string,src:string}){
+    const [open,setOpen] = useState(false)
+    const handleClick = ()=>{
+        setOpen(true)
+    }
 
     return(
+        <>
+        {open && <ImgMask setOpen = {setOpen} src = {src}/>}
         <div className={styles.container}>
-            <div className={styles.image}>
+            
+            <div className={styles.image} onClick={handleClick}>
                 <Image src={src} alt={description} fill sizes="100%" style={{ objectFit: 'cover' }}/>
             </div>
             <div className={styles.title}>
@@ -15,5 +24,6 @@ export default function ProductCard({description,src}:{description:string,src:st
             </div>
             
         </div>
+        </>
     )
 }
